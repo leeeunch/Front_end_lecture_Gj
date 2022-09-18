fc63cb5c553157bb86f2808bc35d6b367c28cb5c
이은찬 <921c@naver.com>
2022-09-19 00:54:45 +0900

    0919

--- 03.JavaScript/ch07.표준내장객체/04.Date 객체.js
+++ 03.JavaScript/ch07.표준내장객체/04.Date 객체.js
@@ -6,23 +6,22 @@ console.log(today.toDateString()); //날짜, 요일
 console.log(today.toLocaleString()); //timezone 반영한 현재 시간
 
 
-function myLocalTimeFormat(date) {
-    let result = '';
-    year = date.getFullYear() % 100;
-    result = `${year<10 ? '0'+year : String(year)}` //3항 연산자
-}
+// function myLocalTimeFormat(date) {
+//     let result = '';
+//     year = date.getFullYear() % 100;
+//     result = `${year<10 ? '0'+year : String(year)}` //3항 연산자
+// }
 
 //22-09-14 09:16:45
 function myLocalTimeFormat(date) {
-    let result = '';
-    let year = String(date.getFullYear() % 100).substring(2); //substring() 공부 //2022 -> 22
-    let month = String(today.getMonth() + 1); //9월 -> 09
+    let year = String(date.getFullYear()).substring(2); //substring() 공부 //2022 -> 22
+    let month = String(date.getMonth() + 1); //9월 -> 09
     month = month.length == 1 ? '0'+month : month;
     let day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
     let hour = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
     let minute = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
     let sec = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
-    return `${year} - ${month} - ${day}`
+    return `${year}-${month}-${day} ${hour}:${minute}:${sec}`;
 }
 
 console.log(myLocalTimeFormat(today));
@@ -38,10 +37,11 @@ console.log(myLocalTimeFormat(strDate));
 
 //시간 요소로 생성
 const elementDate = new Date(2022, 9-1, 14, 9, 50);
-console.log(myLocalTimeFormat());
+console.log(myLocalTimeFormat(elementDate));
 
 //시간 더하고 빼기
 today.setDate(today.getDate()+100);
+console.log('시간 더하고 빼기');
 console.log(myLocalTimeFormat(today));
 
 //시간 간격
